009a88f802c7a0a3e66dc08bb557319ffe02d851
wangxuan <wx_0322@163.com>
2021-03-04 20:47:43 +0800

    更新多语言，更新默认的语言环境为中文

--- aspnet-core/src/KP.FunOA.EntityFrameworkCore/EntityFrameworkCore/Seed/Host/DefaultSettingsCreator.cs
+++ aspnet-core/src/KP.FunOA.EntityFrameworkCore/EntityFrameworkCore/Seed/Host/DefaultSettingsCreator.cs
@@ -30,7 +30,7 @@ namespace KP.FunOA.EntityFrameworkCore.Seed.Host
             AddSettingIfNotExists(EmailSettingNames.DefaultFromDisplayName, "mydomain.com mailer", tenantId);
 
             // Languages
-            AddSettingIfNotExists(LocalizationSettingNames.DefaultLanguage, "en", tenantId);
+            AddSettingIfNotExists(LocalizationSettingNames.DefaultLanguage, "zh-Hans", tenantId);
         }
 
         private void AddSettingIfNotExists(string name, string value, int? tenantId = null)
